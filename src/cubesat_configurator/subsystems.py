89c224b6c2543c84a046a698d06d6860ff0818b7
Nicolas Oidtmann <n.c.oidtmann@student.tudelft.nl>
2024-06-07 23:42:01 +0200

    cleaned up some dublicated code in subsystems.py

--- src/cubesat_configurator/subsystems.py
+++ src/cubesat_configurator/subsystems.py
@@ -1,18 +1,14 @@
 from parapy.core import *
 from parapy.geom import *
-from parapy.core.validate import OneOf, LessThan, GreaterThan, GreaterThanOrEqualTo
+from parapy.core.validate import OneOf, LessThan, GreaterThan, GreaterThanOrEqualTo, Range
 from cubesat_configurator import subsystem as ac
 
 
 class Payload(ac.Subsystem):
-    ##### MOVE EVERYTHING TO INSTRUMENT CLASS #####
     #instrument requirements
     instrument_min_operating_temp = Input() # deg C
     instrument_max_operating_temp = Input() # deg C
     #instrument characteristics
-    #### data_rate is a Attribute and inputs are image pixel resolution 
-    #### and pixel depth and number of images per day
-    # instrument_data_rate = Input() # kbps
     instrument_focal_length = Input() # mm
     instrument_pixel_size = Input() # µm
     instrument_power_consumption = Input() # W
@@ -22,13 +18,20 @@ class Payload(ac.Subsystem):
     instrument_height = Input() # mm
     instrument_width = Input() # mm
     instrument_length = Input() # mm
-    ### maybe we can delete these and use just height, width and length?
     # I feel like images per day should be on mission level?
-    instrument_images_per_day=Input() # number
-    instrument_pixel_resolution=Input() # range to be defined or we split this into w and h, consider list
-    instrument_bit_depth=Input() #range to be defined (1-24) Check gs for inputvalidator
-    
+    instrument_images_per_day=Input() #number
+    instrument_image_width=Input() #pixels
+    instrument_image_height=Input() #pixels #range to be defined or we split this into w and h, consider list
+    instrument_bit_depth=Input(validator=Range(min=1, max=24)) #range to be defined (1-24) Check gs for inputvalidator
+
     height = Input(instrument_height)
+
+    @Attribute
+    def pixel_count(self):
+        """
+        Calculates the number of pixels in the image based on the instrument pixel resolution.
+        """
+        return(self.instrument_image_height*self.instrument_image_width)
 
     # Thought it would be nice to check if the sensor fits within the limits of the satellite
     # I saw in extreme cases (15 µm & 4k resolution) it can be almost 60 mm long on the long side. 
@@ -38,16 +41,6 @@ class Payload(ac.Subsystem):
         Calculates the length of the sensor / processor side based on the resolution and pixel size.
         """
         return max(self.instrument_pixel_resolution)*self.instrument_pixel_size*10**-3 # mm
-
-    @Attribute
-    def pixel_count(self):
-        """
-        Calculates the number of pixels in the image based on the instrument pixel resolution.
-        """
-        # if we use a list:
-        return self.instrument_pixel_resolution[0]*self.instrument_pixel_resolution[1]
-        # if we use separate values for width and height:
-        # return self.instrument_resolution_width*self.instrument_resolution_height
 
     @Attribute
     def instrument_data_rate(self):
@@ -81,58 +74,4 @@ class Structure(ac.Subsystem):
 
 class Thermal(ac.Subsystem):
     pass
-
-
-
-
-
-from parapy.core import *
-from parapy.geom import *
-from parapy.core.validate import OneOf, LessThan, GreaterThan, GreaterThanOrEqualTo, IsInstance, Between
-import subsystem as ac
-    
-
-class Payload(ac.Subsystem):
-    #instrument requirements
-    instrument_min_operating_temp = Input() # deg C
-    instrument_max_operating_temp = Input() # deg C
-    #instrument characteristics
-    instrument_focal_length = Input() # mm
-    instrument_pixel_size = Input() # µm
-    instrument_power_consumption = Input() # W
-    instrument_mass = Input() # kg
-    instrument_height = Input() # mm
-    instrument_width = Input() # mm
-    instrument_length = Input() # mm
-    instrument_cost = Input() # USD
-    instrument_images_per_day=Input() #number
-    instrument_image_width=Input() #pixels
-    instrument_image_height=Input() #pixels #range to be defined or we split this into w and h, consider list
-    instrument_bit_depth=Input(validator=Between(min=1, max=24)) #range to be defined (1-24) Check gs for inputvalidator
-
-    @Attribute
-    def pixel_count(self):
-        return(self.instrument_image_height*self.instrument_image_width)
-
-    @Attribute
-    def instrument_data_rate(self):
-        return (self.pixel_count*self.instrument_bit_depth*self.instrument_images_per_day)/8000
-
-class OBC(ac.Subsystem):
-    pass
-
-
-class EPS(ac.Subsystem):
-    pass
-
-
-class ADCS(ac.Subsystem):
-    pass
-
-
-class COMM(ac.Subsystem):
-    pass
-
-
-
 
