23524137c840dbe38f623a82467c84577bd84ea3
Nicolas Oidtmann <n.c.oidtmann@student.tudelft.nl>
2024-11-24 12:00:13 +0100

    simulate only 1 day & new output files created

--- src/cubesat_configurator/constants.py
+++ src/cubesat_configurator/constants.py
@@ -8,7 +8,7 @@ class PaseosConfig:
     simulation_timestep = 60  # seconds
     start_epoch = pk.epoch_from_string("2024-august-01 08:00:00")
     earth = pk.planet.jpl_lp("earth")
-    days_to_simulate = 2  # days
+    days_to_simulate = 1  # days
     relative_plots_path = os.path.join(script_dir, 'plots', 'simulation_plots.png')
     simulation_plots_location = os.path.join(script_dir, relative_plots_path)
     relative_animation_output_path = os.path.join('animations', 'orbit_animation')
